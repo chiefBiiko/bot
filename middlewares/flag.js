58f76df780d1b26d0e06e8a6e3740db9a49fff6a
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-09-09 11:17:08 +0200

    reflag

--- middlewares/flag.js
+++ middlewares/flag.js
@@ -4,30 +4,30 @@ module.exports = (e, next) => {
   Object.keys(e.stash.hitProducts).forEach(product => {
     e.stash.hitProducts[product].flags = {
       features:
-        RegExp(`features?.{0,13}(\s+of\s+the)?\s+${product}`)
+        RegExp(`features?.{0,13}(\s*of\s*the)?\s*${product}`)
           .test(e.text),
       pictures:
-        RegExp(`pictures?.{0,13}(\s+(of)|(for)\s+the)?\s+${product}`)
+        RegExp(`pictures?.{0,13}(\s*(of)|(for)\s*the)?\s*${product}`)
           .test(e.text),
       price:
-        RegExp(`(price.{0,13}(\s+(of)|(for)\s+the)?)|(costs\s+the)\s+` +
+        RegExp(`(price.{0,13}(\s*(of)|(for)\s*the)?)|(costs\s*the)\s*` +
                `${product}`)
           .test(e.text),
       ratings:
-        RegExp(`(ratings?)|(reviews?).{0,13}(\s+(of)|(for)\s+the)?\s+` +
+        RegExp(`(ratings?)|(reviews?).{0,13}(\s*(of)|(for)\s*the)?\s*` +
                `${product}`)
           .test(e.text),
       wantsMinRating:
-        RegExp(`(min)|(minimum)|(worst)|(badd?est)\s+(rating)|(review).{0,13}` +
-               `(\s+(of)|(for)\s+the)?\s+${product}`)
+        RegExp(`(min)|(minimum)|(worst)|(badd?est)\s*(rating)|(review).{0,13}` +
+               `(\s*(of)|(for)\s*the)?\s*${product}`)
           .test(e.text),
       wantsMaxRating:
-        RegExp(`(max)|(maximum)|(best)|(highest)\s+(rating)|(review).{0,13}` +
-               `(\s+(of)|(for)\s+the)?\s+${product}`)
+        RegExp(`(max)|(maximum)|(best)|(highest)\s*(rating)|(review).{0,13}` +
+               `(\s*(of)|(for)\s*the)?\s*${product}`)
           .test(e.text),
       wantsAvgRating:
-        RegExp(`(avg)|(average)|(mean)|(median)|(mid(dle)?)\s+` +
-               `(rating)|(review).{0,13}(\s+(of)|(for)\s+the)?\s+${product}`)
+        RegExp(`(avg)|(average)|(mean)|(median)|(mid(dle)?)\s*` +
+               `(rating)|(review).{0,13}(\s*(of)|(for)\s*the)?\s*${product}`)
           .test(e.text)
     }
   })
