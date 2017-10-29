eca21dea40f137d1943aad33266750ccda381734
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-10-29 20:55:37 +0100

    got numb

--- pepper-factory/index.js
+++ pepper-factory/index.js
@@ -2,19 +2,27 @@
 // params: string[] of the names of all required parameters in the same order as
 // they appear in func's signature!
 const pepperFactory = (func, paramNames, thisArg) => {
-  const _argmap = paramNames.reduce((acc, cur) => {
-    acc[cur] = { value: undefined, ready: false }
-    return acc
-  }, {})
-  const pepper = argmap => {
-    pepper.peppering = true
-    Object.keys(argmap)
-      .filter(key => _argmap.hasOwnProperty(key))
-      .forEach(key => _argmap[key] = { value: argmap[key], ready: true })
-    return Object.keys(_argmap).every(key => _argmap[key].ready) ?
-      func.apply(thisArg, Object.keys(_argmap).map(key => _argmap[key].value)) :
-        pepper
+  const _args = { 
+    map : {},
+    default: () => paramNames.reduce((acc, cur) => {
+      acc[cur] = { value: undefined, ready: false }
+      return acc
+    }, {}) 
   }
+  const pepper = argmap => {
+    if (!argmap) return
+    Object.keys(argmap)
+      .filter(key => _args.map.hasOwnProperty(key))
+      .forEach(key => _args.map[key] = { value: argmap[key], ready: true })
+    if (Object.keys(_args.map).every(key => _args.map[key].ready)) {
+      const rtn = 
+        func.apply(thisArg, 
+                   Object.keys(_args.map).map(key => _args.map[key].value))
+      _args.map = _args.default()
+      return rtn
+    }
+  }
+  _args.map = _args.default()
   return pepper
 }
 
