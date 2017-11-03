718de87e4473e0db61e6e5a29a871eab18b43b4d
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-11-03 07:50:53 +0100

    aiming

--- dev/pepper5.js
+++ dev/pepper5.js
@@ -15,11 +15,18 @@ const pepperFactory = (func, paramNames, opts) => {
       }, {})
     }
   }
-  const _isObject = x => x && x.__proto__ === Object.prototype
+  // helpers
+  const _isBoolean = x => x && x.__proto__ === Boolean.prototype  
   const _isNumber = x => x && x.__proto__ === Number.prototype
   const _isString = x => x && x.__proto__ === String.prototype
-  const _isBoolean = x => x && x.__proto__ === Boolean.prototype
+  const _isTruthyString = x => _isString(x) && x.length
+  const _isObject = x => x && x.__proto__ === Object.prototype
   const _isArray = x => x && x.__proto__ === Array.prototype
+  const _isNonEmptyArray = x => x && x.__proto__ === Array.prototype && x.length
+  const _isNonEmptyNumberArray = x =>
+    x && x.__proto__ === Array.prototype && x.length && x.every(_isNumber)
+  const _isNonEmptyStringArray = x =>
+    x && x.__proto__ === Array.prototype && x.length && x.every(_isString)
   // walker
   const _walkAndMaybeStash = (obj, n) => {
     if (!opts.levels.includes(-1) && n > Math.max(...opts.levels)) return
@@ -27,7 +34,7 @@ const pepperFactory = (func, paramNames, opts) => {
     //   .filter(key => opts.restrict ? !opts.restrict.test(key) : true)
     //   .map(key => [ key, x[key] ])
       ops.props(obj)
-      .filter(([ key, val ]) => opts.restrict ? !opts.restrict.test(key) : true)
+    //.filter(([ key, val ]) => opts.aims ? !opts.aims.test(key) : true)
       .map(([ key, val ]) => {
         if ((opts.levels.includes(-1) || opts.levels.includes(n)) &&
             _args.map.hasOwnProperty(key) &&
@@ -46,7 +53,7 @@ const pepperFactory = (func, paramNames, opts) => {
     _walkAndMaybeStash(argmap, 0)
     if (Object.keys(_args.map).every(key => _args.map[key].ready)) {
       const rtn =
-        func.apply(opts.thisArg,
+        func.apply(opts.that,
                    Object.keys(_args.map).map(key => _args.map[key].value))
       _args.map = _args.default()
       _input_countr = 0
@@ -72,16 +79,13 @@ const pepperFactory = (func, paramNames, opts) => {
   pepper.getConfig = () => ({ func: func, paramNames: paramNames, opts: opts })
   // defaults
   if (!_isObject(opts)) opts = {}
-  if (!opts.thisArg) opts.thisArg = null
+  if (!opts.that) opts.that = null
   if (!_isBoolean(opts.overwrite)) opts.overwrite = false
-  if (!_isArray(opts.levels) ||
-      !opts.levels.length ||
-      !opts.levels.every(_isNumber))
-    opts.levels = [ 0 ]
+  if (!_isNonEmptyNumberArray(opts.levels)) opts.levels = [ 0 ]
   opts.clearEvery = _isNumber(opts.clearEvery) ?
     Math.floor(opts.clearEvery) : -1
-  opts.restrict = _isString(opts.restrict) && opts.restrict.length ?
-    new RegExp(opts.restrict) : null
+  opts.aims = _isString(opts.aims) && opts.aims.length ?
+    new RegExp(opts.aims) : null
   _args.map = _args.default()
   return pepper
 }
