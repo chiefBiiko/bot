85e415cb615631091c69de8862edb6ae77e4830d
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-11-09 20:43:24 +0100

    clean up

--- dev/pepper-stream.js
+++ dev/pepper-stream.js
@@ -1,29 +1,22 @@
-const pepperFactory = require('.')
-const TransformStream = require('stream').Transform
+const pepperFactory = require('./..')
+const { Transform } = require('stream')
+const is = require('./be')
 
-const pepperStream = (func, paramNames, opts) => {
-  const stream = new TransformStream({
+module.exports = function pepperStream(func, paramNames, opts) {
+  const transformer = new Transform({
     readableObjectMode: true,
     writableObjectMode: true,
     transform(chunk, _, next) {
-      try { // if buffer or string check if JSON
-        chunk = JSON.parse(chunk.toString())
-      } catch(_) {}
+      if (Buffer.isBuffer(chunk) || is.string(chunk)) {
+        try {
+          chunk = JSON.parse(chunk.toString())
+        } catch(_) {}
+      }
       const rtn = this.pepper(chunk)
       if (rtn !== undefined) this.push(rtn)
       next()
     }
   })
-  stream.pepper = pepperFactory(func, paramNames, opts)
-  return stream
+  transformer.pepper = pepperFactory(func, paramNames, opts)
+  return transformer
 }
-
-module.exports = pepperStream
-
-/*********************************************************/
-
-const stringify = (a, b, c) => `a:${a}, b:${b}, c:${c}\n`
-
-process.stdin
-  .pipe(pepperStream(stringify, [ 'a', 'b', 'c' ]))
-    .pipe(process.stdout)
